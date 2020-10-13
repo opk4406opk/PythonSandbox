0208d7497d0a26b14caf970babe3c896fd7e4487
Kuma_jjw <kojeomstudio@gmail.com>
2020-10-13 22:48:10 +0900

    # 작업

--- Practice.py
+++ Practice.py
@@ -27,6 +27,7 @@ myBattleTag = "2LPCCQOUO"
 userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0"
 apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImEzZmIxMGRhLTZmMzgtNGE0Yy1iZDJiLWIzYzI2NTAyYzVkNyIsImlhdCI6MTYwMjMxNzI5MSwic3ViIjoiZGV2ZWxvcGVyL2JlNTE3MjQ2LWNmYzgtZDQ5NC1hOTkxLWNjNGYzOWVhMWNmYSIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTgyLjIyNC43Mi4yMTkiXSwidHlwZSI6ImNsaWVudCJ9XX0.pVNBskTcukhL8wGfvQ00kGBPCCBkse8hFctClFIsNaqWVh8nskJGm47UcWURa56Ti7UIISHEOwbcrwoF8QnkYA"
 url = "https://api.brawlstars.com/v1/players/{0}/battlelog".format(myBattleTag)
+#url = "https://api.brawlstars.com/v1/brawlers" # 브롤러 정보.
 headers = {
 'Content-Type': 'application/json; charset=utf-8',
 'Accept':'application/json',
